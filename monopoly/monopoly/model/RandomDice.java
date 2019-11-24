ede478043ea5e2bafa3f8e46e657b8c73e6fe70a
BehindSoftware <berkaysaydam@iyte.edu.tr>
2019-11-24 12:34:44 +0300

    New Folder Structure

---
+++ monopoly/monopoly/model/RandomDice.java
@@ -0,0 +1,29 @@
+package monopoly.model;
+
+import java.util.Random;
+
+/** Roll a pair of random dice.
+@author Byron Weber Becker */
+public class RandomDice extends Dice
+{
+	/** Construct a new object. */
+	public RandomDice()
+	{  super();
+	}
+
+   /** Return a roll of the pair of dice.
+   @return a number between 2 and 2*numSides */
+   public int getRoll()
+   {  
+    Random random = new Random();
+    int rand = 0;
+    
+    while (true){
+        rand = random.nextInt(13);
+        if(rand !=0) break;
+    }
+    
+    return rand;
+   }
+   
+}
